48d47c8278c2ed5245922ca6cb5399e2391ac82d
SHGroup <kshgroup@kshgroup.kr>
2024-07-21 22:18:15 +0900

    feat: ServerSocket 설정

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,12 +5,12 @@ fun main(args: Array<String>) {
     System.err.println("Logs from your program will appear here!")
 
     // Uncomment this block to pass the first stage
-    // var serverSocket = ServerSocket(6379)
-    //
-    // // Since the tester restarts your program quite often, setting SO_REUSEADDR
-    // // ensures that we don't run into 'Address already in use' errors
-    // serverSocket.reuseAddress = true
-    //
-    // serverSocket.accept() // Wait for connection from client.
-    // println("accepted new connection")
+     var serverSocket = ServerSocket(6379)
+
+     // Since the tester restarts your program quite often, setting SO_REUSEADDR
+     // ensures that we don't run into 'Address already in use' errors
+     serverSocket.reuseAddress = true
+
+     serverSocket.accept() // Wait for connection from client.
+     println("accepted new connection")
 }
