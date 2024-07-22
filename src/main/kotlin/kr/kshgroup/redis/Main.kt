ddb96af301db8f1a7a73e99a9a1780eaab6ebdfd
SHGroup <kshgroup@kshgroup.kr>
2024-07-22 22:01:40 +0900

    feat: Connection 당 1 스레드 생성

--- src/main/kotlin/kr/kshgroup/redis/Main.kt
+++ src/main/kotlin/kr/kshgroup/redis/Main.kt
@@ -1,6 +1,20 @@
 package kr.kshgroup.redis
 
 import java.net.ServerSocket
+import java.net.Socket
+
+fun handleClient(sock: Socket) {
+    val reader = sock.getInputStream().bufferedReader()
+    val writer = sock.getOutputStream().bufferedWriter()
+
+    while (true) {
+        if (reader.readLine() == "PING") {
+            writer.write("+PONG\r\n")
+            writer.flush()
+            println("PONG!")
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,16 +27,10 @@ fun main(args: Array<String>) {
     // ensures that we don't run into 'Address already in use' errors
     serverSocket.reuseAddress = true
 
-    val sock = serverSocket.accept() // Wait for connection from client.
-    println("accepted new connection")
-
-    val reader = sock.getInputStream().bufferedReader()
-    val writer = sock.getOutputStream().bufferedWriter()
     while (true) {
-        if (reader.readLine() == "PING") {
-            writer.write("+PONG\r\n")
-            writer.flush()
-            println("PONG!")
-        }
+        val sock = serverSocket.accept() // Wait for connection from client.
+        println("accepted new connection")
+
+        Thread { handleClient(sock) }.start()
     }
 }
