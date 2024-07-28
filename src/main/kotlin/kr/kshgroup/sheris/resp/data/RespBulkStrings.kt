f187bc690894f6faeb8923354c2f2528e1759eeb
SHGroup <kshgroup@kshgroup.kr>
2024-07-29 05:32:02 +0900

    feat: fix bulk string resp model

--- src/main/kotlin/kr/kshgroup/sheris/resp/data/RespBulkStrings.kt
+++ src/main/kotlin/kr/kshgroup/sheris/resp/data/RespBulkStrings.kt
@@ -4,11 +4,11 @@ import kr.kshgroup.sheris.resp.RESP_DATA_DELIMITER
 import kr.kshgroup.sheris.resp.RespData
 import kr.kshgroup.sheris.resp.RespDataType
 
-class RespBulkStrings(val data: Array<String>) : RespData(RespDataType.BULK_STRINGS) {
+class RespBulkStrings(val data: String) : RespData(RespDataType.BULK_STRINGS) {
     override fun getContentString(): String {
         var content = ""
-        content += "${this.data.size}${RESP_DATA_DELIMITER}"
-        content += this.data.joinToString(RESP_DATA_DELIMITER)
+        content += "${this.data.toByteArray().size}${RESP_DATA_DELIMITER}"
+        content += this.data
         return content
     }
 }
