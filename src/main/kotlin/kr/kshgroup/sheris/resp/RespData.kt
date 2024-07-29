e8407a31df7f7f26d53103174d1670bc31d6fde3
SHGroup <kshgroup@kshgroup.kr>
2024-07-29 20:29:00 +0900

    fix: change dataType to public

--- src/main/kotlin/kr/kshgroup/sheris/resp/RespData.kt
+++ src/main/kotlin/kr/kshgroup/sheris/resp/RespData.kt
@@ -2,7 +2,7 @@ package kr.kshgroup.sheris.resp
 
 const val RESP_DATA_DELIMITER = "\r\n"
 
-abstract class RespData(private val dataType: RespDataType) {
+abstract class RespData(val dataType: RespDataType) {
     override fun toString(): String {
         return this.dataType.firstByte + this.getContentString() + RESP_DATA_DELIMITER
     }
