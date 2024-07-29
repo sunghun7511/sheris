83e1cc339c70bd9878856de4a325658d52d6c927
SHGroup <kshgroup@kshgroup.kr>
2024-07-29 20:30:10 +0900

    fix: add echo command validation

--- src/main/kotlin/kr/kshgroup/sheris/command/CommandEcho.kt
+++ src/main/kotlin/kr/kshgroup/sheris/command/CommandEcho.kt
@@ -1,10 +1,11 @@
 package kr.kshgroup.sheris.command
 
+import kr.kshgroup.sheris.resp.RespDataType
 import kr.kshgroup.sheris.resp.data.RespArrays
 
 object CommandEcho : AbstractCommand("ECHO") {
     override fun validate(args: RespArrays): Boolean {
-        return args.data.size == 2
+        return args.data.size == 2 && args.data[1].dataType == RespDataType.BULK_STRINGS
     }
 
     override fun execute(args: RespArrays): CommandResult {
