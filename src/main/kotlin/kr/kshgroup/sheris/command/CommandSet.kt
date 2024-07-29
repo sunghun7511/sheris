051544c6a8ec6f0f439a88d994b9e9d60d59558e
SHGroup <kshgroup@kshgroup.kr>
2024-07-29 20:54:24 +0900

    feat: implement expire in set command

--- src/main/kotlin/kr/kshgroup/sheris/command/CommandSet.kt
+++ src/main/kotlin/kr/kshgroup/sheris/command/CommandSet.kt
@@ -4,16 +4,35 @@ import kr.kshgroup.sheris.data.Storage
 import kr.kshgroup.sheris.resp.RespDataType
 import kr.kshgroup.sheris.resp.data.RespArrays
 import kr.kshgroup.sheris.resp.data.RespBulkStrings
+import kr.kshgroup.sheris.resp.data.RespSimpleErrors
 import kr.kshgroup.sheris.resp.data.RespSimpleStrings
 
 object CommandSet : AbstractCommand("SET") {
     override fun validate(args: RespArrays): Boolean {
-        return args.data.size == 3 && args.data[1].dataType == RespDataType.BULK_STRINGS && args.data[2].dataType == RespDataType.BULK_STRINGS
+        if (args.data.size < 3) return false
+        if (args.data[1].dataType != RespDataType.BULK_STRINGS) return false
+        if (args.data[2].dataType != RespDataType.BULK_STRINGS) return false
+
+        if (args.data.size > 3) {
+            if (args.data[3] !is RespBulkStrings) return false
+        }
+        return true
     }
 
     override fun execute(args: RespArrays): CommandResult {
-        val key = (args.data[1] as RespBulkStrings).data
-        val value = (args.data[2] as RespBulkStrings).data
+        val key = (args.data[1] as RespBulkStrings).getDataOrEmpty()
+        val value = (args.data[2] as RespBulkStrings).getDataOrEmpty()
+
+        if (args.data.size > 3) {
+            val arg = (args.data[3] as RespBulkStrings).getDataOrEmpty()
+            if (arg.lowercase() != "px") return CommandResult(RespSimpleErrors("ERR syntax error: $arg"))
+            if (args.data.size < 5) return CommandResult(RespSimpleErrors("ERR syntax error: missing expire time"))
+
+            val expire = (args.data[4] as RespBulkStrings).data!!.toIntOrNull()
+                ?: return CommandResult(RespSimpleErrors("ERR invalid expire time in set"))
+            Storage.set(key, value, expire)
+            return CommandResult(RespSimpleStrings("OK"))
+        }
 
         Storage.set(key, value)
 
