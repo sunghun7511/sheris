18c53eb6bd260eb282efa79515267b65274b5c5a
SHGroup <kshgroup@kshgroup.kr>
2024-08-12 21:53:11 +0900

    fix: hardcode master replication id

--- src/main/kotlin/kr/kshgroup/sheris/ReplicationManager.kt
+++ src/main/kotlin/kr/kshgroup/sheris/ReplicationManager.kt
@@ -2,13 +2,15 @@ package kr.kshgroup.sheris
 
 import kr.kshgroup.sheris.util.RandomUtil
 
+const val HARD_CODED_REPLICATION_ID = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb"
+
 enum class ReplicationRole(name: String) {
     MASTER("master"),
     SLAVE("slave")
 }
 
 class ReplicationManager(sherisServer: SherisServer, replicationConfiguration: ReplicationConfiguration) {
-    val role: ReplicationRole
+    val role: ReplicationRole = replicationConfiguration.role
 
     // For master
     val masterReplicationId: String
@@ -16,9 +18,9 @@ class ReplicationManager(sherisServer: SherisServer, replicationConfiguration: R
     val connectedSlaves: List<String> = emptyList()
 
     init {
-        role = replicationConfiguration.role
         if (replicationConfiguration.role == ReplicationRole.MASTER) {
-            masterReplicationId = RandomUtil.generateRandomHex(40)
+            masterReplicationId = HARD_CODED_REPLICATION_ID
+//            masterReplicationId = RandomUtil.generateRandomHex(40)
             masterReplicationOffset = 0
         } else {
             throw IllegalArgumentException("Replication role must be MASTER")
